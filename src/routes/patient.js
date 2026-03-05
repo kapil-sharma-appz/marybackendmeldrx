9015fe333f110f6dc13cdc413b9f4479f3471519
kapil sharma <kapil.sharma@appzlogic.com>
2026-03-05 17:10:00 +0530

    updates

--- src/routes/patient.js
+++ src/routes/patient.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controller/patient');
 
+router.route('/:patient_id').get([patientController.getPatient]);
 router.route('/').get([patientController.getPatientList]);
 
-router.route('/:patient_id').get([patientController.getPatient]);
 
 
 
